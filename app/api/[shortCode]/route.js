16f82fcceab390fc94a4c548dc5b516469131c78
Adil C <adilc0070@gmail.com>
2025-01-22 23:42:03 +0530

    route checking

--- app/api/[shortCode]/route.js
+++ app/api/[shortCode]/route.js
@@ -1,21 +1,21 @@
-// app/api/[shortCode]/route.js
-import { NextResponse } from 'next/server';
-import dbConnect from '../../../utils/mongodb';
-import Url from '../../../models/Url';
+// app/[shortCode]/page.js
+import { redirect } from 'next/navigation';
+import dbConnect from '../../utils/mongodb';
+import Url from '../../models/Url';
 
-export async function GET(request, { params }) {
+export default async function ShortCodePage({ params }) {
   const { shortCode } = params;
-  console.log('shortCode', shortCode);
+  
   await dbConnect();
   const url = await Url.findOne({ shortCode });
   
   if (!url) {
-    console.log('URL not found');
-    return NextResponse.json({ error: 'URL not found' }, { status: 404 });
+    redirect('/'); // Redirect to home if short code not found
   }
   
+  // Increment clicks
   url.clicks += 1;
   await url.save();
   
-  return NextResponse.redirect(url.originalUrl);
+  redirect(url.originalUrl);
 }
